94dc485152219d9e8feb2df73359942b514e8be1
shreyanshi-gupta <145694928+shreyanshi-gupta@users.noreply.github.com>
2026-02-12 12:45:41 +0000

    Add backend folder with index.js

---
+++ backend/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 5000;
+
+
+// GET route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Smart Internship Portal Backend is Running 🚀",
+    status: "success"
+  });
+});
+
+
+// GET internships route
+app.get("/internships", (req, res) => {
+  const internships = [
+    {
+      id: 1,
+      title: "Frontend Developer Intern",
+      company: "TechNova",
+      location: "Remote"
+    }
+  ];
+
+  res.json({
+    success: true,
+    count: internships.length,
+    data: internships
+  });
+});
+
+
+// 👇 WRITE POST ROUTE HERE
+app.post("/register", (req, res) => {
+
+  const studentData = req.body;
+
+  res.json({
+    message: "Student registered successfully",
+    data: studentData
+  });
+
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
